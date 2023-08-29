271874438d7924de9840ad4489d05b335a28fa3c
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2023-08-29 14:27:10 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# initialise paths or directories
+DATA_PATH = '/content/data/FBCG'
+TEMP_DIR = '/content/data/templates/Template'
+ZIP_PATH = '/content/drive/Shareddrives/Drive/PhD/FBCG Dataset.zip'
+SAVE_DIR  = '/content/drive/Shareddrives/Drive/PhD/IDC_Grading_PyTorch/best_model'
+RESULT_DIR = '/content/drive/Shareddrives/Drive/PhD/IDC_Grading_PyTorch/record'
